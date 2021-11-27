75ec1caa0c1c4ae84ab36a447f221034682381c7
Xue Jiao <jiao.xue@seamoney.com>
2021-11-27 12:23:35 +0800

    impl ipv4Payload

--- ping.go
+++ ping.go
@@ -1,5 +1,9 @@
 package ping
 
+import (
+	"golang.org/x/net/ipv4"
+)
+
 func byteSliceOfSize(n int) []byte {
 	b := make([]byte, n)
 	for i := 0; i < len(b); i++ {
@@ -7,4 +11,12 @@ func byteSliceOfSize(n int) []byte {
 	}
 
 	return b
+}
+
+func ipv4Payload(b []byte) []byte {
+	if len(b) < ipv4.HeaderLen {
+		return b
+	}
+	hdrlen := int(b[0]&0x0f) << 2 // each bit for 4 bytes, minimum 5 bits
+	return b[hdrlen:]
 }
