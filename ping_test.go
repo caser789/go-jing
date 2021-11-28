b9e963c88b620e756ecd0ad7e3af7cf89626aa4e
Xue Jiao <jiao.xue@seamoney.com>
2021-11-28 21:03:20 +0800

    Fix runtime error of ipv4Payload

--- ping_test.go
+++ ping_test.go
@@ -15,40 +15,6 @@ func Test_byteSliceOfSize(t *testing.T) {
 	assert.Equal(t, []byte{1, 1}, slice)
 }
 
-func Test_ipv4Payload(t *testing.T) {
-	tests := []struct {
-		name  string
-		input []byte
-		want  []byte
-	}{
-		{
-			name:  "no header",
-			input: []byte{1, 2},
-			want:  []byte{1, 2},
-		},
-		{
-			name: "header length 5",
-			input: []byte{
-				0b00000101, 0, 0, 0, 0,
-				1, 1, 1, 1, 1,
-				2, 2, 2, 2, 2,
-				3, 3, 3, 3, 3,
-				4, 4, 4, 4, 4,
-			},
-			want: []byte{
-				4, 4, 4, 4, 4,
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := ipv4Payload(tt.input)
-			assert.Equal(t, tt.want, got)
-		})
-	}
-}
-
 func Test_bytesToTime(t *testing.T) {
 	tm := time.Now()
 	b := timeToBytes(tm)
